26c46b66c3c010061bde6d300d850e547be5966d
chaseknis <chase6th@gmail.com>
2023-08-07 12:49:47 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Industries from './routes/Industries';
+import Services from './routes/Services';
+import Translation from './routes/Translation';
+import FAQ from './routes/FAQ';
+import Contact from './routes/Contact';
 
-function App() {
-  return (
-    <h1>
-      hello
-    </h1>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/industries" element={<Industries />} />
+    <Route path="/services" element={<Services />} />
+    <Route path="/translation" element={<Translation />} />
+    <Route path="/faq" element={<FAQ />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+);
 
 export default App;
---
+++ src/routes/Industries.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Industries = () => (
+  <h1>Industries</h1>
+);
+
+export default Industries;

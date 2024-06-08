9b31cf01fee5d95ada0d7acd158032fba6bcfa29
chaseknis <chase6th@gmail.com>
2024-06-08 02:15:21 +0200

    Removed large files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import videoBg1 from '../assets/videoBg1.mp4';
-// import videoBg2 from '../assets/videoBg2.mp4';
-// import videoBg3 from '../assets/videoBg3.mp4';
-// import videoBg4 from '../assets/videoBg4.mp4';
 import videoBg5 from '../assets/videoBg5.mp4';
 import './styles/home.css';
 

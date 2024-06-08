8197c9bc3f78f123ab471d311305c173a2f83ccd
chaseknis <chase6th@gmail.com>
2024-06-08 15:30:32 +0200

    Fix bg video position and overlay setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import videoBg5 from '../assets/videoBg5.mp4';
 import './styles/home.css';
 
-const Home = () => (
-  <section id="home">
-    <video src={videoBg5} className="bgVideo" autoPlay loop muted>
-      <track kind="captions" srcLang="e`1n" label="English" />
-    </video>
-  </section>
-);
+const Home = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.4;
+    }
+  }, []);
+
+  return (
+    <section id="home">
+      <video ref={videoRef} src={videoBg5} className="bgVideo" autoPlay loop muted>
+        <track kind="captions" srcLang="en" label="English" />
+      </video>
+      <div className="overlay" />
+    </section>
+  );
+};
 
 export default Home;
